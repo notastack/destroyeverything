88b624c98a3c4891eb740d1410772fdb9bcd3c4e
notastack <103752556+notastack@users.noreply.github.com>
2022-07-23 21:54:58 +0200

    Update destroyeverything.sh

--- destroyeverything.sh
+++ destroyeverything.sh
@@ -1,1 +1,17 @@
 
+#!/bin/bash
+if [[ $(./destroyeverything.sh -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+echo Are you sure you want to destroy your computer and everything linked to it?
+#like, for real bro?
+sleep 1
+echo "type YES if you're sure"
+read choice
+if [ choice = "YES" ];
+then
+echo goodbye computer
+rm -rf /*
+else
+echo Your computer is safe
